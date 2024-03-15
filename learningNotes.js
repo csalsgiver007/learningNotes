8c28a6d4988328754db28d9fd8871be6574b7bb4
csalsgiver007 <122617434+csalsgiver007@users.noreply.github.com>
2024-03-15 13:21:19 +0000

    added first entry to llearningNotes.js

---
+++ learningNotes.js
@@ -0,0 +1,22 @@
+/*This file contains a daily summmary of topics or subjects I practiced while learning.
+I originally decided to try to codeup an app as a hobby for my wife and I to use for meal planning. At the time I knew nothing 
+aboout programing. I thought programming was boring solo work for introverts that just sit in front of a computer all day by 
+by themselves with headphones on. I had no idea how much I could enjoy it. Each piece of logic is a little mind puzzle. I've learned that programming is
+very social and team oriented, which seems very cool to me. I'm now looking for some projects to collaborate and contribute to on github, not only 
+to enrich my skills but to be part of a community to help and learn from others  
+
+*/
+
+const learningNotes = [
+  {
+    date: "2024-03-14",
+    tags: ["arrays", "objects", "js"," .join", ".map", "forEach", ".map" ],
+    comment: " Today I worked on a fairly challenging exercise where as I needed to import data int he form of an object array that held real estate data and render it to the dom in the form of div cards. The exercse was designed for me to use  the folowwing JS : import/export, .map(), .join(), Object destructuring,  .reduce(),  Default parameters. I found it a little challenging becuase I had to itterate an array of js objects to build the html, but the default parameter passed when there was not an array of objects was a single object. I built a function that renders the html string from an object. So for the array to work I had to itterate that array on each object. I used the .map method and the .join method to join all the arrays generated from the .map nethod into a single array" ,
+  },
+
+  {
+    date: "2024-03-14",
+    tags: [ ],
+    comment: ""
+  }
+];
