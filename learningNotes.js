affea94798159ad7934e36c9d89c178d79f2e1bc
csalsgiver007 <122617434+csalsgiver007@users.noreply.github.com>
2024-03-20 09:25:14 +0000

    added image field to learningNotes

--- learningNotes.js
+++ learningNotes.js
@@ -12,36 +12,65 @@ const learningNotes = [
     date: "2024-03-14",
     tags: ["arrays", "objects", "js"," .join", ".map", "forEach", ".map" ],
     comment: " Today I worked on a fairly challenging exercise where as I needed to import data int he form of an object array that held real estate data and render it to the dom in the form of div cards. The exercse was designed for me to use  the folowwing JS : import/export, .map(), .join(), Object destructuring,  .reduce(),  Default parameters. I found it a little challenging becuase I had to itterate an array of js objects to build the html, but the default parameter passed when there was not an array of objects was a single object. I built a function that renders the html string from an object. So for the array to work I had to itterate that array on each object. I used the .map method and the .join method to join all the arrays generated from the .map nethod into a single array" ,
+    liveUrl: "",
+    repo: "",
+    designFile:"",
+    image:"",
   },
 
   {
     date: "2024-03-15",
     tags: [ "ternary", "rest parameter", "spread operator", "short circuit", "switch statements", "constructors"],
-    comment: "This morning I worked on js exercises utilizing ternary operator, rest parameter, short circuiting, switch statement, and started to build constructors "
+    comment: "This morning I worked on js exercises utilizing ternary operator, rest parameter, short circuiting, switch statement, and started to build constructors ",
+    liveUrl: "",
+    repo: "",
+    designFile:"",
+    image:"",
   },
   {
     date: "2024-03-18",
     tags: ["constructor", "this keyword" ],
-    comment: "This morning I practiced more with constructors and the .this keyword"
+    comment: "This morning I practiced more with constructors and the .this keyword",
+    liveUrl: "",
+    repo: "",
+    designFile:"",
+    image:"",
+  },
+  {
+    date: "2024-03-19",
+    tags: [".map", "data-sets", ".join", "" ],
+    comment: "Began working on an exercise to build a mock food ordering app. The app takes an array from an external data file and renders a list of menu items using JS. An eventlistener listens for clicks on an order button and adds the item to an order. You can then check out, where a modal opens to take your name and payment information. Building te design from scratch without starter css and html is slightly challenging since I'm still not the fastest at styling html. I'm trying to conform as close as possible to the given figma design",  
+    liveUrl: "",
+    repo: "https://github.com/csalsgiver007/restaurantOrderingApp",
+    designFile:"https://www.figma.com/file/Hdgwo69Dym9vVsxbuPbl0h/Mobile-Restaurant-Menu?type=design&node-id=0-1&mode=design&t=aAhmX1JIeL5BX4ms-0",
+    image:"",
+
   },
   {
     date: "2024-03-15",
     tags: [ ],
-    comment: ""
+    comment: "",
+    liveUrl: "",
+    repo: "",
+    designFile:"",
+    image:"",
   },
   {
     date: "2024-03-15",
     tags: [ ],
-    comment: ""
+    comment: "",
+    liveUrl: "",
+    repo: "",
+    designFile:"",
+    image:"",
   },
   {
     date: "2024-03-15",
     tags: [ ],
-    comment: ""
-  },
-  {
-    date: "2024-03-15",
-    tags: [ ],
-    comment: ""
+    comment: "",
+    liveUrl: "",
+    repo: "",
+    designFile:"",
+    image:"",
   },
 ];
